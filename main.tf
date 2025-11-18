7022a00c3a6edf57f3682bbcd3395fa55cf6c337
hakkaoui <hamza.akkaoui@oracle.com>
2025-11-18 19:25:41 +0100

    remove s3

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "eu-central-1"
 }
-module "s3" {
-  source = "./modules/s3"
-  s3_bucket_name = "my-terraform-state-bucket"
-}
+# module "s3" {
+#   source = "./modules/s3"
+#   s3_bucket_name = "my-terraform-state-bucket"
+# }
 
 
