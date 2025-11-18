3ed1f0cf341ba186999e1997cad92e09ce9f0465
hakkaoui <hamza.akkaoui@oracle.com>
2025-11-18 19:11:24 +0100

    remove access_key from provider

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-  type = string
-}
-variable "secret_key" {
-  type = string
-}
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
